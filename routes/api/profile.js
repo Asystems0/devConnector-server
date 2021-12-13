094a15f254994586c6d1e1e37c0f8deebc945cd4
Asaf <asafsadika7@gmail.com>
2021-12-13 17:43:29 +0200

    Remove posts from user deleted

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,7 @@ dotenv.config();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -150,7 +151,8 @@ router.get("/user/:user_id", async (req, res) => {
 // @access  private
 router.delete("/", auth, async (req, res) => {
   try {
-    // @tofo - remove users posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
 
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
